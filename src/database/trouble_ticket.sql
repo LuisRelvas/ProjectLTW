b8bc191503a6569d877d2d0ded5df04557ca1b2c
LuisRelvas <luisrelvas@netcabo.pt>
2023-04-20 15:58:56 +0100

    Updated trouble_ticket.sql

--- src/database/trouble_ticket.sql
+++ src/database/trouble_ticket.sql
@@ -20,7 +20,9 @@ CREATE TABLE user(
 	username TEXT NOT NULL,
 	name TEXT NOT NULL,
 	email TEXT NOT NULL,
-	password NVARCHAR(60) NOT NULL);
+	password TEXT NOT NULL
+	--- CONSTRAINT user_pk PRIMARY KEY(id)
+);
 
 CREATE TABLE ticket(
 	ticket_id INTEGER PRIMARY KEY,
@@ -30,107 +32,97 @@ CREATE TABLE ticket(
 	tittle TEXT NOT NULL,
 	description TEXT NOT NULL,
 	initial_date DATE NOT NULL,
-	agent_id INTEGER DEFAULT -1, 
 	FOREIGN KEY(id) REFERENCES user(id),
 	FOREIGN KEY(department_id) REFERENCES department(department_id),
 	FOREIGN KEY(status_id) REFERENCES status(status_id)
+	--- CONSTRAINT ticket_pk PRIMARY KEY(ticket_id)
 );
 
 CREATE TABLE status(
-	status_id INTEGER,
+	status_id INTEGER PRIMARY KEY,
 	name TEXT NOT NULL,
 	open INTEGER NOT NULL DEFAULT 0,
 	assigned INTEGER NOT NULL DEFAULT 0,
-	closed INTEGER NOT NULL DEFAULT 0,
-	CONSTRAINT status_pk PRIMARY KEY(status_id)
+	closed INTEGER NOT NULL DEFAULT 0
+	--- CONSTRAINT status_pk PRIMARY KEY(status_id)
 );
 
 
 CREATE TABLE department(
-	department_id INTEGER,
-	name TEXT NOT NULL,
-	CONSTRAINT department_pk PRIMARY KEY(department_id)
+	department_id INTEGER PRIMARY KEY,
+	name TEXT NOT NULL
+	--- CONSTRAINT department_pk PRIMARY KEY(department_id)
 );
 
 CREATE TABLE hashtag(
-	hashtag_id INTEGER,
-	tag TEXT NOT NULL UNIQUE,
-	CONSTRAINT hashtag_pk PRIMARY KEY(hashtag_id)
+	hashtag_id INTEGER PRIMARY KEY,
+	tag TEXT NOT NULL UNIQUE
+	--- CONSTRAINT hashtag_pk PRIMARY KEY(hashtag_id)
 
 );
 
 CREATE TABLE ticketHashtag(
-	ticket_id INTEGER,
-    hashtag_id INTEGER,
+		ticket_id INTEGER NOT NULL,
+    hashtag_id INTEGER NOT NULL,
     FOREIGN KEY (ticket_id) REFERENCES ticket(ticket_id),
     FOREIGN KEY (hashtag_id) REFERENCES hashtag(hashtag_id),
-    CONSTRAINT ticketHastag_pk PRIMARY KEY (ticket_id, hashtag_id)
+    PRIMARY KEY (ticket_id, hashtag_id)
 );
 
 CREATE TABLE reply( 
-	ticket_id INTEGER,
-	department_id INTEGER,
+	ticket_id INTEGER NOT NULL,
+	department_id INTEGER NOT NULL,
 	FOREIGN KEY (ticket_id) REFERENCES ticket(ticket_id), 
 	FOREIGN KEY (department_id) REFERENCES department(department_id),
-	CONSTRAINT reply_pk PRIMARY KEY(ticket_id,department_id)
+	PRIMARY KEY(ticket_id,department_id)
 );
 
 CREATE TABLE faq(
-	faq_id INTEGER,
+	faq_id INTEGER PRIMARY KEY,
 	question TEXT NOT NULL,
-	answer TEXT NOT NULL,
-	CONSTRAINT faq_pk PRIMARY KEY (faq_id)
+	answer TEXT NOT NULL
+	--- CONSTRAINT faq_pk PRIMARY KEY (faq_id)
 );
 
 CREATE TABLE ticketFaq(
-	ticket_id INTEGER,
-	faq_id INTEGER, 
+	ticket_id INTEGER NOT NULL,
+	faq_id INTEGER NOT NULL, 
 	FOREIGN KEY (ticket_id) REFERENCES ticket(ticket_id),
 	FOREIGN KEY (faq_id) REFERENCES faq(faq_id),
-	CONSTRAINT ticketFaq_pk PRIMARY KEY (ticket_id, faq_id)
+	PRIMARY KEY (ticket_id, faq_id)
 );
 
-INSERT INTO user(id, role, username, name, email, password) VALUES (0, 0, 'andyDC', 'Angy Pita', 'angy.da.cruz@hotmail.com', '$2y$10$pq95wdIQVm0i1.LP098tHeka5KsyGA9wiw6yX2lcpeyEgJboFQMOi');
-INSERT INTO user(id, role, username, name, email, password) VALUES (1, 0, 'dawnofdom', 'Domingos Santos', 'domingosjsmsantos@gmail.com', '$2y$10$pq95wdIQVm0i1.LP098tHeka5KsyGA9wiw6yX2lcpeyEgJboFQMOi');
-INSERT INTO user(id, role, username, name, email, password) VALUES (2, 0, 'luisrelvas', 'Luis Relvas', 'luisrelvas@hotmail.com', '$2y$10$pq95wdIQVm0i1.LP098tHeka5KsyGA9wiw6yX2lcpeyEgJboFQMOi');
+INSERT INTO user(role, username, name, email, password) VALUES (0, 'andyDC', 'Angy Pita', 'angy.da.cruz@hotmail.com', '$2y$10$pq95wdIQVm0i1.LP098tHeka5KsyGA9wiw6yX2lcpeyEgJboFQMOi');
+INSERT INTO user(role, username, name, email, password) VALUES (0, 'dawnofdom', 'Domingos Santos', 'domingosjsmsantos@gmail.com', '$2y$10$pq95wdIQVm0i1.LP098tHeka5KsyGA9wiw6yX2lcpeyEgJboFQMOi');
+INSERT INTO user(role, username, name, email, password) VALUES (0, 'luisrelvas', 'Luis Relvas', 'luisrelvas@hotmail.com', '$2y$10$pq95wdIQVm0i1.LP098tHeka5KsyGA9wiw6yX2lcpeyEgJboFQMOi');
 
-INSERT INTO user(id, role, username, name, email, password) VALUES (3, 1, 'lucianas', 'Luciana Silva', 'lucianasilva@gmail.com', '$2y$10$pq95wdIQVm0i1.LP098tHeka5KsyGA9wiw6yX2lcpeyEgJboFQMOi');
-INSERT INTO user(id, role, username, name, email, password) VALUES (4, 1, 'josemiguel', 'José Miguel', 'josemiguel@gmail.com', '$2y$10$pq95wdIQVm0i1.LP098tHeka5KsyGA9wiw6yX2lcpeyEgJboFQMOi');
-INSERT INTO user(id, role, username, name, email, password) VALUES (5, 1, 'andreasousa', 'Andréa Sousa', 'andreasousa@gmail.com', '$2y$10$pq95wdIQVm0i1.LP098tHeka5KsyGA9wiw6yX2lcpeyEgJboFQMOi');
-INSERT INTO user(id, role, username, name, email, password) VALUES (6, 1, 'carolinap', 'Carolina Pinto', 'carolinapinto@gmail.com', '$2y$10$pq95wdIQVm0i1.LP098tHeka5KsyGA9wiw6yX2lcpeyEgJboFQMOi');
-INSERT INTO user(id, role, username, name, email, password) VALUES (7, 1, 'pedroalves', 'Pedro Alves', 'pedroalves@gmail.com', '$2y$10$pq95wdIQVm0i1.LP098tHeka5KsyGA9wiw6yX2lcpeyEgJboFQMOi');
+INSERT INTO user(role, username, name, email, password) VALUES (1, 'lucianas', 'Luciana Silva', 'lucianasilva@gmail.com', '$2y$10$pq95wdIQVm0i1.LP098tHeka5KsyGA9wiw6yX2lcpeyEgJboFQMOi');
+INSERT INTO user(role, username, name, email, password) VALUES (1, 'josemiguel', 'José Miguel', 'josemiguel@gmail.com', '$2y$10$pq95wdIQVm0i1.LP098tHeka5KsyGA9wiw6yX2lcpeyEgJboFQMOi');
+INSERT INTO user(role, username, name, email, password) VALUES (1, 'andreasousa', 'Andréa Sousa', 'andreasousa@gmail.com', '$2y$10$pq95wdIQVm0i1.LP098tHeka5KsyGA9wiw6yX2lcpeyEgJboFQMOi');
+INSERT INTO user(role, username, name, email, password) VALUES (1, 'carolinap', 'Carolina Pinto', 'carolinapinto@gmail.com', '$2y$10$pq95wdIQVm0i1.LP098tHeka5KsyGA9wiw6yX2lcpeyEgJboFQMOi');
+INSERT INTO user(role, username, name, email, password) VALUES (1, 'pedroalves', 'Pedro Alves', 'pedroalves@gmail.com', '$2y$10$pq95wdIQVm0i1.LP098tHeka5KsyGA9wiw6yX2lcpeyEgJboFQMOi');
 
-INSERT INTO user(id, role, username, name, email, password) VALUES (8, 2, 'josecunha', 'José Cunha', 'josecunha@gmail.com', '$2y$10$pq95wdIQVm0i1.LP098tHeka5KsyGA9wiw6yX2lcpeyEgJboFQMOi');
-INSERT INTO user(id, role, username, name, email, password) VALUES (9, 2, 'mariajose', 'Maria José', 'mariajose@gmail.com', '$2y$10$pq95wdIQVm0i1.LP098tHeka5KsyGA9wiw6yX2lcpeyEgJboFQMOi');
-INSERT INTO user(id, role, username, name, email, password) VALUES (10, 2, 'luisgomes', 'Luís Gomes', 'luisgomes@gmail.com', '$2y$10$pq95wdIQVm0i1.LP098tHeka5KsyGA9wiw6yX2lcpeyEgJboFQMOi');
-INSERT INTO user(id, role, username, name, email, password) VALUES (11, 2, 'andreoliveira', 'André Oliveira', 'andreoliveira@gmail.com', '$2y$10$pq95wdIQVm0i1.LP098tHeka5KsyGA9wiw6yX2lcpeyEgJboFQMOi');
-INSERT INTO user(id, role, username, name, email, password) VALUES (12, 2, 'margaridasantos', 'Margarida Santos', 'margaridasantos@gmail.com', '$2y$10$pq95wdIQVm0i1.LP098tHeka5KsyGA9wiw6yX2lcpeyEgJboFQMOi');
+INSERT INTO user(role, username, name, email, password) VALUES (2, 'josecunha', 'José Cunha', 'josecunha@gmail.com', '$2y$10$pq95wdIQVm0i1.LP098tHeka5KsyGA9wiw6yX2lcpeyEgJboFQMOi');
+INSERT INTO user(role, username, name, email, password) VALUES (2, 'mariajose', 'Maria José', 'mariajose@gmail.com', '$2y$10$pq95wdIQVm0i1.LP098tHeka5KsyGA9wiw6yX2lcpeyEgJboFQMOi');
+INSERT INTO user(role, username, name, email, password) VALUES (2, 'luisgomes', 'Luís Gomes', 'luisgomes@gmail.com', '$2y$10$pq95wdIQVm0i1.LP098tHeka5KsyGA9wiw6yX2lcpeyEgJboFQMOi');
+INSERT INTO user(role, username, name, email, password) VALUES (2, 'andreoliveira', 'André Oliveira', 'andreoliveira@gmail.com', '$2y$10$pq95wdIQVm0i1.LP098tHeka5KsyGA9wiw6yX2lcpeyEgJboFQMOi');
+INSERT INTO user(role, username, name, email, password) VALUES (2, 'margaridasantos', 'Margarida Santos', 'margaridasantos@gmail.com', '$2y$10$pq95wdIQVm0i1.LP098tHeka5KsyGA9wiw6yX2lcpeyEgJboFQMOi');
 -- Add more user entries as needed
 
 
-INSERT INTO department(department_id, name) VALUES (0, 'IT');
-INSERT INTO department(department_id, name) VALUES (1, 'HR');
+INSERT INTO department(name) VALUES ('IT');
+INSERT INTO department(name) VALUES ('HR');
 -- Add more department entries as needed
 
-INSERT INTO status(status_id, name, open, assigned, closed) VALUES (0, 'Open', 1, 0, 0);
-INSERT INTO status(status_id, name, open, assigned, closed) VALUES (1, 'Assigned', 0, 1, 0);
-INSERT INTO status(status_id, name, open, assigned, closed) VALUES (2, 'Closed', 0, 0, 1);
+INSERT INTO status(name, open, assigned, closed) VALUES ('Open', 1, 0, 0);
+INSERT INTO status(name, open, assigned, closed) VALUES ('Assigned', 0, 1, 0);
+INSERT INTO status(name, open, assigned, closed) VALUES ('Closed', 0, 0, 1);
 -- Add more status entries as needed
 
-INSERT INTO hashtag(hashtag_id, tag) VALUES (0, 'urgent');
-INSERT INTO hashtag(hashtag_id, tag) VALUES (1, 'important');
+INSERT INTO hashtag(tag) VALUES ('urgent');
+INSERT INTO hashtag(tag) VALUES ('important');
 -- Add more hashtag entries as needed
 
-INSERT INTO faq(faq_id, question, answer) VALUES (0, 'What is the ticket submission process?', 'You can submit a ticket through the ticket submission form on the website.');
-INSERT INTO faq(faq_id, question, answer) VALUES (1, 'How do I reset my password?', 'You can reset your password by clicking on the "Forgot Password" link on the login page.');
+INSERT INTO faq(question, answer) VALUES ('What is the ticket submission process?', 'You can submit a ticket through the ticket submission form on the website.');
+INSERT INTO faq(question, answer) VALUES ('How do I reset my password?', 'You can reset your password by clicking on the "Forgot Password" link on the login page.');
 -- Add more faq entries as needed
-
-INSERT INTO ticket(id, department_id, status_id, tittle, description, initial_date) VALUES (0, 1, 1, 'Test', 'Teste', '2023-04-19');
-INSERT INTO ticket(id, department_id, status_id, tittle, description, initial_date) VALUES (0, 1, 1, 'Test', 'Teste', '2023-04-19');
-INSERT INTO ticket(id, department_id, status_id, tittle, description, initial_date) VALUES (1, 1, 1, 'Test', 'Teste', '2023-04-19');
-INSERT INTO ticket(id, department_id, status_id, tittle, description, initial_date) VALUES (2, 1, 1, 'Test', 'Teste', '2023-04-19');
-INSERT INTO ticket(id, department_id, status_id, tittle, description, initial_date) VALUES (3, 1, 1, 'Test', 'Teste', '2023-04-19');
-INSERT INTO ticket(id, department_id, status_id, tittle, description, initial_date) VALUES (4, 1, 1, 'Test', 'Teste', '2023-04-19');
-INSERT INTO ticket(id, department_id, status_id, tittle, description, initial_date) VALUES (5, 1, 1, 'Test', 'Teste', '2023-04-19');
-INSERT INTO ticket(id, department_id, status_id, tittle, description, initial_date) VALUES (6, 1, 1, 'Test', 'Teste', '2023-04-19');
-INSERT INTO ticket(id, department_id, status_id, tittle, description, initial_date) VALUES (7, 1, 1, 'Test', 'Teste', '2023-04-19');
